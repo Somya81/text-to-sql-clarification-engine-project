36a7a4eef57717b9ba151961c9e5e157096c9c91
Somya Nema <somyanema18@gmail.com>
2026-09-11 23:48:09 +0530

    Add SQL query execution function

--- app/database.py
+++ app/database.py
@@ -4,14 +4,20 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-connection = mysql.connector.connect(
-    host=os.getenv("DB_HOST"),
-    user=os.getenv("DB_USER"),
-    password=os.getenv("DB_PASSWORD"),
-    database=os.getenv("DB_NAME")
-)
+def execute_query(sql):
+    connection = mysql.connector.connect(
+        host=os.getenv("DB_HOST"),
+        user=os.getenv("DB_USER"),
+        password=os.getenv("DB_PASSWORD"),
+        database=os.getenv("DB_NAME")
+    )
 
-if connection.is_connected():
-    print("MySQL connection successful!")
+    cursor = connection.cursor()
+    cursor.execute(sql)
 
-connection.close()
+    results = cursor.fetchall()
+
+    cursor.close()
+    connection.close()
+
+    return results
