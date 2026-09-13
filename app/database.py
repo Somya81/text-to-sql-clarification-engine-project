ca51520e19ebc562a78977bbb0ba9a1dc2670bce
Somya Nema <somyanema18@gmail.com>
2026-09-13 17:22:22 +0530

    Added Validation for SELECT and WITH queries allowed

--- app/database.py
+++ app/database.py
@@ -5,6 +5,18 @@ from dotenv import load_dotenv
 load_dotenv()
 
 def execute_query(sql):
+
+    sql=sql.strip().rstrip(";").strip()
+    upper_sql=sql.upper()
+    if upper_sql.startswith("SELECT"):
+        pass
+    elif upper_sql.startswith("WITH") and "SELECT" in upper_sql:
+        pass
+    else:
+        raise ValueError(
+            "Only SELECT and WITH... SELECT queries are allowed."
+        )
+
     connection = mysql.connector.connect(
         host=os.getenv("DB_HOST"),
         user=os.getenv("DB_USER"),
